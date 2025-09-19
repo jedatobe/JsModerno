be81386f97e93dc35ee6507bc2d72e0b78518ccf
WappsiDesarrolloWeb2 <wappsi.desarrolloweb02@gmail.com>
2025-09-19 11:12:16 -0500

    Seccion 3: Operadores

--- 1-lecciones-curso-javascript-moderno/3.-Operadores/js/aplicacion5.js
+++ 1-lecciones-curso-javascript-moderno/3.-Operadores/js/aplicacion5.js
@@ -1,7 +1,7 @@
 // OPERADORES LÓGICOS
 
 // Operador lógico OR
-let valor1 = true
+/* let valor1 = true
 let valor2 = false
 console.log(valor1 || valor1); // true
 console.log(valor1 || valor2); // true
@@ -27,3 +27,42 @@ let nombre
 let apellido
 let nombreusuario = `admin`
 console.log(nombre || apellido || nombreusuario);
+
+let valor3
+let valor4 = null
+let valor5 = 0
+console.log(valor3 || valor4 || valor5); // Si todos son falsos devuelve el valor del último operando
+
+let c = 1
+let d = 10
+let e = 20
+console.log(c || d || e); // Si todos son verdaderos devuelve el valor del primer operando */
+
+// Operador lógico AND
+/* let f = true
+let g = false
+console.log(f && f); // true
+console.log(f && g); // false
+console.log(g && f); // false
+console.log(g && g); // false
+
+// Operador AND con tipos de datos distintos a boleanos
+console.log(0 && undefined);//0 si ambos operando son falsos devuelve el primer operando
+console.log(null && 5);     //0 devuelve el primer operando: null
+console.log(12 && -1);      //0 devuelve el segundo operando: -1
+console.log(20 && 25);      //0 si ambos operando son verdaderos devuelve el segundo operando: 25
+
+//  Multiples valores aplicados al operador AND
+console.log(1 && 2 && null);        // retorna el primer valor false: null
+console.log(0 && -1 && undefined);  // retorna el primer valor false: 0
+console.log(1 && 2 && 3);           // retorna el último valor verdadero: 3 */
+
+/* // Operador NOT
+let h = 100
+let i = 50
+console.log(!(h == i)); // true */
+
+// Operador ternario
+let edad = 21
+let evaluar = (edad >= 18) ? `Eres mayor de edad` : `No eres mayor de edad`
+console.log(evaluar);
