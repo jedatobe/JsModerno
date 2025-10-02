976c14ba75546098ac3d8fd22771f5b18054b2ca
DanDev <jedatobe@gmail.com>
2025-10-01 21:20:19 -0500

    Seccion 8: Arreglos | Métodos para bscar elementos en un arreglo

--- 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion2.js
+++ 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion2.js
@@ -1,9 +1,18 @@
 // AÑADIR O ELIMINAR ELEMENTOS
+const animalesMarinos = [`Estrella de mar`, `Pulpo`, `Delfin`, `Manatí`]
+console.log(animalesMarinos);
 
 // Método push(): añade uno o más elementos al final del arreglo. (Sintaxis: arr.push(element1[, ...[, elementN]]))
+console.log(animalesMarinos.push(`Cangrejo Ermitaño`));
+
+console.log(animalesMarinos.push(`Calamar`, `Ballena Azul`));
 
 // Método unshift(): agrega uno o más elementos al inicio del array. (Sintaxis: arr.unshift(elemento1[, ...[, elementoN]]))
+console.log(animalesMarinos.unshift(`Tiburón Blanco`));
+console.log(animalesMarinos.unshift(`Lenguado común`, `Cangrejo guisante`));
 
 // Método pop(): elimina el último elemento de un array. (Sintaxis: arr.pop())
+console.log(animalesMarinos.pop());
 
 // Método shift(): elimina el primer elemento de un array. (Sintaxis: arr.shift())
+console.log(animalesMarinos.shift());
--- 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion4.js
+++ 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion4.js
@@ -2,13 +2,31 @@
 
 /* Método includes() determina si un array incluye un determinado elemento, 
 devuelve true o false según corresponda. (Sintaxis: arr.includes(searchElement[, fromIndex])) */
+const productos = [`lavaplatos`, `lavadoras`, `estufas`, `refrigeradores`, `placas de cocción`]
+console.log(productos);
+console.log(productos.includes(`horno de pared`));   // false
+console.log(productos.includes(`lavadoras`));        // true
+console.log(productos.includes(`lavadoras`, 1));     // true
+console.log(productos.includes(`refrigeradores`, 5));// false
 
 /* Método indexOf(): retorna el primer índice en el que se encuentra un
 elemento dado en el array, ó retorna -1 si el elemento no esta presente. 
  (Sintaxis: arr.indexOf(searchElement[, fromIndex]))
 */
+const numeros = [50, 100, 115, 25, 2, 115, 100, 50]
+console.log(numeros);
+console.log(numeros.indexOf(100));
+console.log(numeros.indexOf(20));
+console.log(numeros.indexOf(100, 2));
+console.log(numeros.indexOf(115, 3));
 
 /* Método lastIndexOf(): devuelve el último índice en el que un elemento
 puede encontrarse en el arreglo, ó -1 si el elemento no esta presente. El
 arreglo es recorrido en sentido contrario, empezando por el índice fromIndex.
 (Sintaxis: arr.lastIndexOf(searchElement) / arr.lastIndexOf(searchElement, fromIndex)) */
+const animales = [`elefante`, `leon`, `tigre`, `hipopotamo`, `leon`, `elefante`]
+console.log(animales);
+console.log(animales.lastIndexOf(`leon`));
+console.log(animales.lastIndexOf(`elefante`));
+console.log(animales.lastIndexOf(`canguro`));
+console.log(animales.lastIndexOf(`hipopotamo`, 3));
