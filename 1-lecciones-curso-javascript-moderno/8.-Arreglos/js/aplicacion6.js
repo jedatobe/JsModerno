8f58510cb29167fb303648f73afc3eb57e444bac
DanDev <jedatobe@gmail.com>
2025-10-02 21:11:56 -0500

    Sección 8: Desestructuración de arreglos

--- 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion6.js
+++ 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion6.js
@@ -1,5 +1,9 @@
 // ARREGLOS ANIDADOS
+const shoppingCart = ['tablet', 'monitores', 'laptop', 'desktop', ['procesadores', 'trajeta madre', 'tarjeta gráfica', 'memorias']]
+console.log(shoppingCart);
 
 // Acceder al arreglo anidado
+console.log(shoppingCart[4]);
 
 // Acceder a un elemento del arreglo anidado
+console.log(shoppingCart[4][2]);
--- 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion7.js
+++ 1-lecciones-curso-javascript-moderno/8.-Arreglos/js/aplicacion7.js
@@ -1,10 +1,62 @@
 // DESESTRUCTURACIÓN DE ARREGLOS EN JAVASCRIPT
 
+// JS clásico
+// const producto = ["tablet", "laptop", "desktop", "monitor", "teclado", "mouse"];
+// const tablet = producto[0]
+// const laptop = producto[1]
+// const desktop = producto[2]
+
+// console.log(producto);
+// console.log(tablet);
+// console.log(laptop);
+// console.log(desktop);
+
+// JS Moderno (Destructuración)
+const producto = ["tablet", "laptop", "desktop", "monitor", "teclado", "mouse"];
+const [primero, segndo, tercero] = producto
+
+console.log(producto);
+console.log(primero);
+console.log(segndo);
+console.log(tercero);
+
 // Asignación separada de la declaración
+// JS clásico
+// let frutas, frutas2
+// frutas = 'kiwi'
+// frutas2 = 'mango'
+
+// console.log(frutas);
+// console.log(frutas2);
+
+// JS Moderno (Destructuración)
+let frutas, frutas2
+[frutas, frutas2] = ['kiwi', 'mango']
+console.log(frutas);
+console.log(frutas2);
 
 // Valores predeterminados
+let nombre, apellido
+[nombre, apellido = 'smith'] = ['Meghan']
+console.log(nombre);
+console.log(apellido);
 
 // Intercambio de variables
+// JS clásico
+// let a = 10
+// let b = 30
+// let auxiliar = a
+// a = b
+// b = auxiliar
+// console.log(a);
+// console.log(b);
+
+// JS Moderno (Destructuración)
+let a = 10;
+let b = 30;
+[a, b] = [b, a];
+console.log(a);
+console.log(b);
 
 // OPERADOR SPREAD Y SINTAXIS EL RESTO
 
