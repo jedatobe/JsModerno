62a0dedb9fa98d0c34511ed09f27112974f65789
DanDev <jedatobe@gmail.com>
2025-11-04 22:27:51 -0500

    Sección 11: Bucles e iteradores

--- 1-lecciones-curso-javascript-moderno/11.-Bucles-iteradores/js/aplicacion3.js
+++ 1-lecciones-curso-javascript-moderno/11.-Bucles-iteradores/js/aplicacion3.js
@@ -2,4 +2,20 @@
 
 // Break
 
+// Continue// DECLARACIONES BREAK Y CONTINUE
+
+// Break
+// for(let i = 0; i <= 100; i++){
+//     console.log(i);
+//     if(i == 10){
+//         break;
+//     }
+// }
+
 // Continue
+for(let i = 0; i <= 20; i++){
+    if(i == 3){
+        continue
+    }
+    console.log(i);
+}
--- 1-lecciones-curso-javascript-moderno/11.-Bucles-iteradores/js/aplicacion8.js
+++ 1-lecciones-curso-javascript-moderno/11.-Bucles-iteradores/js/aplicacion8.js
@@ -2,3 +2,7 @@
 
 /* Método forEach: Ejecuta la función definida por cada uno de los elementos del array.
 (Sintaxis: arr.forEach(function(item, index, array) {...});) */
+const oceanos = ["Atlántico", "Pacífico", "Índico", "Ártico", "Antártico",];
+oceanos.forEach((oceano, index) => {
+    console.log(index + " " + oceano);
+});
