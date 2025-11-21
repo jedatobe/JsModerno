1dd5be16c6028b2b6666b281d5970c710105e356
DanDev <jedatobe@gmail.com>
2025-11-20 20:39:21 -0500

    Seccion 12 Fechas

--- 1-lecciones-curso-javascript-moderno/12.-Fechas-horas/js/aplicacion1.js
+++ 1-lecciones-curso-javascript-moderno/12.-Fechas-horas/js/aplicacion1.js
@@ -12,10 +12,21 @@ new Date(year,month,day,hours,minutes,seconds,ms)
 new Date(milliseconds) */
 
 // new Date(): Sin argumentos – crea un objeto Date para la fecha y la hora actuales:
+const date = new Date();
+console.log(date);
 
 // new Date(date string): Convierte automáticamente un string a fecha.
+const dateString = new Date('2030/03/12');
+console.log(dateString);
+
+const dateString2 = new Date('March 12, 2030');
+console.log(dateString2);
 
 /* new Date(year,month,day,hours,minutes,seconds,ms): Crea una fecha con los componentes pasados 
 como argumentos en la zona horaria local. */
+const a = new Date(2050,11);
+console.log(a);
 
 // Autocorrección
+const b = new Date(2035,05,32,16,35,03);
+console.log(b);
--- 1-lecciones-curso-javascript-moderno/12.-Fechas-horas/js/aplicacion2.js
+++ 1-lecciones-curso-javascript-moderno/12.-Fechas-horas/js/aplicacion2.js
@@ -1,9 +1,17 @@
 // MODIFICAR EL FORMATO PREDETERMINADO DE LAS FECHAS Y HORAS
 
 // toDateString(): devuelve formato sólo de fecha
+const date = new Date(2025, 01, 09);
+console.log(date.toDateString());
 
 // toUTCString(): devuelve la fecha utilizando formato UTC
+const d = new Date();
+console.log(d.toUTCString());
 
 // toTimeString(): devuelve formato sólo de hora
+// const hora = new Date();
+// console.log(hora.toTimeString());
 
 // toLocaleTimeString(): devuelve formato sólo de hora, pero en el formato regional actual
+const hora = new Date();
+console.log(hora.toLocaleTimeString('es-CO'));
