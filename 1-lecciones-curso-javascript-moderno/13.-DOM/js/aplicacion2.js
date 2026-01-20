32dff13bee77ca058f9d7e53de1b5ac902614ec6
DanDev <jedatobe@gmail.com>
2026-01-19 20:26:21 -0500

    Seccion 13: Modelo del objeto Document | Seleccionar elementos de DOM (método moderno)

--- 1-lecciones-curso-javascript-moderno/13.-DOM/js/aplicacion2.js
+++ 1-lecciones-curso-javascript-moderno/13.-DOM/js/aplicacion2.js
@@ -1,9 +1,26 @@
 // SELECCIONAR ELEMENTOS DEL DOM (MÉTODOS TRADICIONALES)
 
 // Método getElementsByClassName(): busca elementos con el atributo class especificado.
+let elementos = document.getElementsByClassName("item")
+console.log(elementos);
+console.log(`El tercer elemento es: ` + elementos[2].innerHTML);
+
+elementos = document.getElementsByClassName("audio")
+console.log(elementos);
 
 // Método getElementsByTagName(): busca elementos HTML por su etiqueta de elemento HTML.
+let elementos2 = document.getElementsByTagName("h2")
+console.log(elementos2);
+console.log(`El número de elementos <h2> es igual a: `+elementos2.length);
 
 // Método getElementsByName(): busca elementos con el atributo name.
+let elementos3 = document.getElementsByName("calificacion")
+console.log(elementos3);
+
+let elementos4 = document.getElementById("destacado")
+console.log(elementos4);
+console.log(elementos4.innerHTML);
+
+
 
 // Método getElementById(): busca un elemento HTML con el atributo ID especificado.
--- 1-lecciones-curso-javascript-moderno/13.-DOM/js/aplicacion3.js
+++ 1-lecciones-curso-javascript-moderno/13.-DOM/js/aplicacion3.js
@@ -1,5 +1,48 @@
 // SELECCIONAR ELEMENTOS DEL DOM (MÉTODOS MODERNOS)
 
 // Método querySelector(): busca el primer elemento que coincide con el selector CSS especificado.
+let item = document.querySelector('*')
+console.log(item);
+
+item = document.querySelector('h2')
+console.log(item);
+
+item = document.querySelector('.menu')
+console.log(item);
+
+item = document.querySelector('#principal')
+console.log(item);
+
+item = document.querySelector('div p')
+console.log(item);
+
+item = document.querySelector('a[href]')
+console.log(item);
+
+item = document.querySelector('a[href="acerca.html"]')
+console.log(item);
+
+item = document.querySelector('ul > li')
+console.log(item);
+
+item = document.querySelector('h2 + p')
+console.log(item);
+
+item = document.querySelector('li:nth-child(2)')
+console.log(item);
+
+item = document.querySelector('section .info')
+console.log(item);
 
 // Método querySelectorAll(): busca todos los elementos que coinciden con el selector CSS especificado.
+let items = document.querySelectorAll('*')
+console.log(items);
+
+items = document.querySelectorAll('h2')
+console.log(items);
+
+items = document.querySelectorAll('.menu')
+console.log(items);
+
+items = document.querySelectorAll('section .info')
+console.log(items);
